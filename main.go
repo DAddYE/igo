d009def9329ff6886ef33c381988564b3d606aff
Davide D'Agostino <info@daddye.it>
2014-03-25 17:31:44 -0700

    Parse the output of gobuild and match line/column.

--- main.go
+++ main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
 	"os"
 	"os/exec"
 	"path"
+	"path/filepath"
+	"regexp"
 	"runtime"
+	"strconv"
 	"strings"
 
 	"github.com/DAddYE/igo/cmd"
@@ -42,6 +46,55 @@ func toCmd(c string) Cmd {
 	return Cmd(0)
 }
 
+func abs(i int) int {
+	switch {
+	case i < 0:
+		return -i
+	default:
+		return i
+	}
+}
+
+func parseError(err []byte) {
+	const regex = `^(.*):(\d+):(\d+)?\s*(.*)`
+	re := regexp.MustCompile(regex)
+
+	// Iterate over each error message.
+	for _, line := range bytes.Split(err, []byte{'\n'}) {
+
+		// if the error message is kind of:
+		//		./path/name.go:line:col error message
+		//
+		match := re.FindAllStringSubmatch(string(line), 4)
+		if len(match) == 1 {
+			file, _ := filepath.Rel("./", match[0][1]) // TODO: check this under chdir
+			line, _ := strconv.Atoi(match[0][2])
+			col, _ := strconv.Atoi(match[0][3])
+			message := match[0][4]
+			igoFile := strings.TrimSuffix(file, ".go") + ".igo"
+			if pos := cmd.IgoPositions[igoFile]; pos != nil {
+				var cols []int
+				for in, out := range *pos {
+					if out.Line == line {
+						cols = append(cols, in.Column)
+					}
+				}
+				if len(cols) > 0 {
+					closest := abs(cols[0] - col)
+					for _, c := range cols {
+						if cd := abs(c - col); cd < closest {
+							closest = cd
+						}
+					}
+					fmt.Printf("%s:%d:%d %s\n", igoFile, line, closest, message)
+				} else {
+					fmt.Printf("%s:%d:%d %s\n", igoFile, line, col, message)
+				}
+			}
+		}
+	}
+}
+
 func main() {
 	flag.Usage = usage
 	flag.Parse()
@@ -71,11 +124,11 @@ func main() {
 		exitCode = cmd.To(cmd.GO, paths)
 		if exitCode == 0 {
 			gocmd := path.Join(runtime.GOROOT(), "bin", "go")
-			fmt.Println("Building...")
 			out, err := exec.Command(gocmd, "build").CombinedOutput()
 			if err != nil {
-				os.Stderr.Write(out)
-				fmt.Printf("%v\n", err)
+				// os.Stderr.Write(out)
+				parseError(out)
+				exitCode = 1
 			}
 		}
 	default:
